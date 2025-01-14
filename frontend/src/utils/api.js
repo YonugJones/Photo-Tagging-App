206e6d198102e34a4a04a00045cb8005febacf4e
YonugJones <peterckerfoot@gmail.com>
2025-01-14 16:52:32 -0600

    added getScores and postScore api function

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,10 @@
 // Functions for API calls (validate user guess and fetch high scores)
 
-const BASE_API = 'http://localhost:3000';
+const API_URL = 'http://localhost:3000';
 
 export const validateClick = async (clickX, clickY, characterName) => {
   try {
-    const response = await fetch(`${BASE_API}/validate`, {
+    const response = await fetch(`${API_URL}/validate`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -20,5 +20,46 @@ export const validateClick = async (clickX, clickY, characterName) => {
   } catch (err) {
     console.error('Error validating click', err);
     return { success: false, message: 'Error occurred while validating click' }
+  }
+};
+
+export const getScores = async () => {
+  try {
+    const response = await fetch(`${API_URL}/scores`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch scores');
+    }
+    
+    return await response.json();
+  } catch (err) {
+    console.error('Error fetching scores:', err);
+    return { success: false, message: 'Error occurred while fetching scores' };
+  }
+};
+
+export const postScores = async (playerName, time) => {
+  try {
+    const response = await fetch(`${API_URL}/scores`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify({ playerName, time }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to post score');
+    }
+    
+    return await response.json();
+  } catch (err) {
+    console.error('Error posting score:', err);
+    return { success: false, message: 'Error occurred while posting score' };
   }
 };
