7f96bd4603db054d3adb09f1f2d42465e2144ec4
YonugJones <peterckerfoot@gmail.com>
2025-01-14 10:50:24 -0600

    created index.js file and structured

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const errorHandler = require('./middleware/errorHandler');
+const app = express();
+
+// Application level middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.use('/', (req, res) => {
+  res.json({ message: 'Index' })
+})
+
+// Global error handler
+app.use(errorHandler);
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
