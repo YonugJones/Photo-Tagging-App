71dea5a3e40e0e8ccbc328a9b889103af8317bda
YonugJones <peterckerfoot@gmail.com>
2025-01-14 16:39:16 -0600

    added postScore function

--- backend/controllers/scoreController.js
+++ backend/controllers/scoreController.js
@@ -4,20 +4,46 @@ const CustomError = require('../errors/customError');
 
 const getScores = asyncHandler(async (req, res) => {
   const scores = await prisma.score.findMany({
-    orderBy: { time: 'desc' },
+    orderBy: { time: 'asc' },
     select: {
       playerName: true,
       time: true
     }
   })
 
+  if (!scores) {
+    throw new CustomError('Failed to find scores', 404);
+  }
+
   res.status(200).json({
     success: true,
     message: 'All scores retrieved',
     data: scores
   })
-}) 
+});
+
+const postScore = asyncHandler(async (req, res) => {
+  const { playerName, time } = req.body;
+
+  if (!playerName || !time) {
+    throw new CustomError('Player name and time are required', 400);
+  }
+
+  const score = await prisma.score.create({
+    data: {
+      playerName,
+      time,
+    }
+  })
+
+  res.status(200).json({
+    success: true,
+    message: 'Score submitted',
+    data: score
+  })
+})
 
 module.exports = {
-  getScores
+  getScores,
+  postScore
 }
--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const scoreController = require('../controllers/scoreController');
 
 router.get('/', scoreController.getScores);
+router.post('/', scoreController.postScore);
 
 module.exports = router;
