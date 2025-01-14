0aef3719af587b95af05f75c88c0cd3911b3221b
YonugJones <peterckerfoot@gmail.com>
2025-01-14 15:13:02 -0600

    created Timer component

--- frontend/src/components/Timer.jsx
+++ frontend/src/components/Timer.jsx
@@ -1,1 +1,19 @@
 // Timer component
+
+import { useTimer } from '../context/TimerContext';
+
+const Timer = () => {
+  const { elapsedTime } = useTimer();
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  }
+
+  return (
+    <div className='timer'>
+      <p>Time: {formatTime(elapsedTime)}</p>
+    </div>
+  )
+}
