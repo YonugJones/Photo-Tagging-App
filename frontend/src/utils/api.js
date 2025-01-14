d385f864c290016b94dd445666965caf84788a7a
YonugJones <peterckerfoot@gmail.com>
2025-01-14 12:36:29 -0600

    created api function for validateClick

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,1 +1,24 @@
 // Functions for API calls (validate user guess and fetch high scores)
+
+const BASE_API = 'http://localhost:3000';
+
+export const validateClick = async (clickX, clickY, characterName) => {
+  try {
+    const response = await fetch(`${BASE_API}/validate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ clickX, clickY, characterName }),
+    });
+    
+    if (!response.ok) {
+      throw new Error('Failed to validate click');
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error('Error validating click', err);
+    return { success: false, message: 'Error occurred while validating click' }
+  }
+};
